61ab891da5638b385fa3b4ff9da9fd0b1a05f9ab
iyedtkachuk <iyedtkachuk@gmail.com>
2023-08-06 11:20:05 +0200

    fixed typo

--- learning_projet/dbtlearn/models/fct/fct_reviews.sql
+++ learning_projet/dbtlearn/models/fct/fct_reviews.sql
@@ -12,5 +12,5 @@ where review_text is not null
 -- i need to tell dbt how to increment, what records are right and how dbt knows that the records are new or not
 {% if is_incremental() %}
 -- am only interested on records under this conditions
-  and review_date >= (select max(review_date) from {{ this }})
+  and review_date > (select max(review_date) from {{ this }})
 {% endif %}
